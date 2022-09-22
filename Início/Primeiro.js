4a79c59b70974b4c115bb341376c3eeb16bb0db1
Maxwell <maxwellpires2002@gmail.com>
2022-09-22 11:06:31 -0300

    Sentenças e Blocos de código - Atv. 1

--- Início/Primeiro.js
+++ Início/Primeiro.js
@@ -1,3 +1,19 @@
 //Comentários igauis ao utilizados na linguagem C
 /*Realmente os mesmos*/
 console.log("Olá, Mundo");
+
+//Atividade 1 - Exibir os digitos impares, depois os pares
+{
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
+}
+{
+    console.log("0");
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+}
