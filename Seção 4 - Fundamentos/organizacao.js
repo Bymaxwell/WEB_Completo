d0b1154e5de2d4a767a50b86665933e0db8ccbd0
Maxwell <maxwellpires2002@gmail.com>
2022-09-23 11:31:38 -0300

    Tipo numérico

--- Seção 4 - Fundamentos/organizacao.js
+++ Seção 4 - Fundamentos/organizacao.js
@@ -24,5 +24,22 @@ let texto = "Legal";
 console.log(texto);
 console.log(typeof texto);
 
+//Tipo numérico
+const peso1 = 1.0;
+const peso2 = Number("2.0");
+
+console.log("peso1, peso2");
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avaliacao1 = 9.871;
+const avaliacao2 = 6.871;
+
+const media = (avaliacao1*peso1+avaliacao2*peso2)/(peso1+peso2);
+console.log(media.toFixed(2)); //Duas casas decimais
+console.log(media.toString(2)); //Converte para  uma base determinada
+console.log(typeof media);
+
+
 
 
