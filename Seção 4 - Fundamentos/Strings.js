e226023bb6c382da42a9db7c15b29aa181a2b879
Maxwell <maxwellpires2002@gmail.com>
2022-09-23 11:58:26 -0300

    Tipo String

---
+++ Seção 4 - Fundamentos/Strings.js
@@ -0,0 +1,11 @@
+const escola = "UFMA";
+console.log(escola.charAt(2));
+console.log(escola.charCodeAt(2));
+console.log(escola.indexOf('A'));
+
+console.log(escola.substring(1));
+
+console.log('Escola '.concat(escola).concat(" !"));
+console.log(escola.replace('A', 'K'));    
+
+console.log('Maria, João, José'.split(','));
