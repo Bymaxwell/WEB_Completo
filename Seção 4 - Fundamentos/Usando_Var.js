0dc9515785f43677b29e47d49fdb19e0bcdb79df
Maxwell <maxwellpires2002@gmail.com>
2022-09-24 11:49:46 -0300

    Diferença entre var e let

--- Seção 4 - Fundamentos/Usando_Var.js
+++ Seção 4 - Fundamentos/Usando_Var.js
@@ -17,5 +17,28 @@ function teste()  {
 }
 
 teste();
-console.log(local); //Retorna um erro uma vez que a variável "local" é de escopo local dentro da função "teste"
+// console.log(local); Retorna um erro uma vez que a variável "local" é de escopo local dentro da função "teste"
 //Dessa forma a variável do tipo VAR só tem dois escopos: ou local ou global
+
+//EXEMPLO com var
+
+var numero = 1;
+{
+    var numero = 2;
+    console.log("dentro = ", numero); 
+}
+
+console.log("fora = ", numero);
+
+//EXEMPLO com let
+
+let numero_1 = 1;
+{
+    let numero_1 = 2;
+    console.log("dentro = ", numero_1); 
+}
+
+console.log("fora = ", numero_1);
+
+//O let te escopo no local em que está inserido, se ali não for conter a variável, ele procura em um escopo acima
+
