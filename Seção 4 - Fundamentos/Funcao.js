ce1b010a7701786f80934f7b9ab26e7d66c6d731
Maxwell <maxwellpires2002@gmail.com>
2022-09-24 11:22:47 -0300

    Exemplo de Funções_01

--- Seção 4 - Fundamentos/Funcao.js
+++ Seção 4 - Fundamentos/Funcao.js
@@ -4,3 +4,25 @@ class Produto {};
 console.log(typeof Produto);
 
 //Java Script é altamente centrado em funções. Muito importante entender esse conceito
+
+//Exemplos Funções sem retorno
+
+function imprimeSoma(a, b){
+    console.log(a+b);
+}
+imprimeSoma(45, 45);
+imprimeSoma(2);
+imprimeSoma(2, 3, 4, 5, 6, 7);
+imprimeSoma();
+
+//Exemplos Funções com retorno
+
+function soma(a, b=1){
+    return a+b;
+}
+
+console.log(soma(5, 6));
+console.log(soma(2));
+
+
+
