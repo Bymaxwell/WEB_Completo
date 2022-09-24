9d56b654816b360602d1a94463aa72fa5dbf55c8
Maxwell <maxwellpires2002@gmail.com>
2022-09-24 10:28:22 -0300

    Tipo Objeto e notações

---
+++ Seção 4 - Fundamentos/Objeto.js
@@ -0,0 +1,20 @@
+const produto_1 = {};
+produto_1.nome = "Celular ULTRA";
+produto_1.preco = 2345.89;
+
+console.log(produto_1);
+console.log(produto_1.nome);
+console.log(produto_1.preco);
+
+const profuto_2 ={
+    nome: "Camisa Polo",
+    preco: 78.89,
+    objeto: {
+        propriedade1 :  1,
+        propriedade2:   2,
+        objeto: {
+            propriedade1: 3,
+            propriedade2: 4
+        }
+    }
+}
