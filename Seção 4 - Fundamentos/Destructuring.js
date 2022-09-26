9ddbc3f393825ce22a494982e59f10e43fa808fb
Maxwell <maxwellpires2002@gmail.com>
2022-09-26 11:32:54 -0300

    Destructuring em Array

--- Seção 4 - Fundamentos/Destructuring.js
+++ Seção 4 - Fundamentos/Destructuring.js
@@ -20,5 +20,14 @@ console.log(sobrenome);
 const {endereco: {logradouro, numero, bairro}} = pessoa;
 console.log(logradouro, numero, bairro);
 
+// Destructuring 2
 
+const [a, b, c, d] = [1, 2, 3, 4]
+console.log(a, b, c , d);
+
+const [e, f, , g, h] = [9, 8, 7, 6]
+console.log(e, f, g, h);
+
+const [, [, segundo_segundo]] = [[1, 2, 3], [4, 5, 6]]
+console.log(segundo_segundo);
 
