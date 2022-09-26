33ee633b27b8b2f91bf604abdf6371820cdbdd87
Maxwell <maxwellpires2002@gmail.com>
2022-09-26 11:47:05 -0300

    Destructuring em Funções

--- Seção 4 - Fundamentos/Destructuring.js
+++ Seção 4 - Fundamentos/Destructuring.js
@@ -31,3 +31,29 @@ console.log(e, f, g, h);
 const [, [, segundo_segundo]] = [[1, 2, 3], [4, 5, 6]]
 console.log(segundo_segundo);
 
+//Destructuring 3/4
+
+function rand({min=0, max=1000}){
+    const valor = Math.random()*(max-min)+min;
+    return Math.floor(valor);
+}
+
+console.log(rand({max: 50, min: 40}))
+console.log(rand({}));
+//console.log(rand()); Erro de compilação
+
+function rand_2([min=0, max = 1000]){
+    if(min > max) [min, max] = [max, min]
+    const valor = Math.random()*(max-min)+min;
+    return Math.floor(valor);
+}
+
+console.log(rand_2([50, 40]));
+console.log(rand_2([]));
+//console.log(rand_2()); Também retorna um erro
+
+
+
+
+
+
