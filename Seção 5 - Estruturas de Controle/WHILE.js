0d969d2a7eaf31313d31639cd1a2d0e685c3cad6
Maxwell <maxwellpires2002@gmail.com>
2022-09-28 10:34:02 -0300

    Estrutura WHILE

---
+++ Seção 5 - Estruturas de Controle/WHILE.js
@@ -0,0 +1,12 @@
+function Inteiro_Aleatorio(min, max){
+    const valor = Math.random()*(max-min)+min;
+    return Math.floor(valor);
+
+}
+
+let opcao = 0;
+while(opcao != 1){
+    opcao = Inteiro_Aleatorio(-1, 10);
+    console.log(opcao);
+
+}
