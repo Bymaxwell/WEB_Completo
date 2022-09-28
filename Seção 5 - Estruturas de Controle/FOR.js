82cc9e8caa44a4410af6804e0894b98a970f1aef
Maxwell <maxwellpires2002@gmail.com>
2022-09-28 11:16:46 -0300

    Estrutura FOR IN

--- Seção 5 - Estruturas de Controle/FOR.js
+++ Seção 5 - Estruturas de Controle/FOR.js
@@ -13,3 +13,21 @@ const notas = [9.25,2.34,3.67,4.12,5.55,6.29,7.84,8.14,9.16,10]
 for(let i=9; i>-1; i--){
     console.log(notas[i]);
 }
+
+//FOR IN
+console.log('FOR IN')
+for (let i in notas){
+    console.log(notas[i]);
+}
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 19,
+    cor_favorita: 'azul',
+    comida_favorita: 'macarronada'
+}
+
+for(let atributo in pessoa){
+    console.log(`${atributo} = ${pessoa[atributo]}`)
+}
