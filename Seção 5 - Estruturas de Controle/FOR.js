d57a3ff0903a24f163315eb011c5fe8c4498378e
Maxwell <maxwellpires2002@gmail.com>
2022-09-28 11:06:37 -0300

    Estrutura FOR

---
+++ Seção 5 - Estruturas de Controle/FOR.js
@@ -0,0 +1,15 @@
+let contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+const notas = [9.25,2.34,3.67,4.12,5.55,6.29,7.84,8.14,9.16,10]
+
+for(let i=9; i>-1; i--){
+    console.log(notas[i]);
+}
