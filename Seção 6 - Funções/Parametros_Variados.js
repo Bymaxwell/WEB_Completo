16681b232702bf1a82b64727ca9b0a583dc5846b
Maxwell <maxwellpires2002@gmail.com>
2022-09-29 09:37:16 -0300

    Parametros Variados

---
+++ Seção 6 - Funções/Parametros_Variados.js
@@ -0,0 +1,14 @@
+function soma(){
+    let soma = 0;
+    for( i in arguments){
+        soma += arguments[i];
+    }
+    return soma
+}
+
+console.log(soma(1,2));
+console.log(soma(1,2,3));
+console.log(soma(1,2,3,4));
+console.log(soma(1,2,3,4,5));
+console.log(soma(1,2,3,4,5,6));
+console.log(soma('a','b','c'));
