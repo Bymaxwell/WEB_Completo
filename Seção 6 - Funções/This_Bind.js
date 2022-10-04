ea3673b7052f7ab5e96c5d6b0678cee90714d98f
Maxwell <maxwellpires2002@gmail.com>
2022-10-04 08:24:22 -0300

    This_Bind_2

--- Seção 6 - Funções/This_Bind.js
+++ Seção 6 - Funções/This_Bind.js
@@ -9,3 +9,16 @@ falar(); //Conflito entre programação funcional e Orientação a Objetos
 
 const falarDepessoa = pessoa.falar.bind(pessoa)
 falarDepessoa();
+
+//BIND 2
+const self = this;
+function Pessoa(){
+    self.idade = 0;
+
+    setInterval(function(){
+        self.idade++;
+        console.log(self.idade);
+    }/*.bind(this)*/, 1000)
+}
+
+new Pessoa
