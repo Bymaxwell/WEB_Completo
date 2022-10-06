d91e6823bcb347984b8d7d1ca6007bec82f7d05f
Maxwell <maxwellpires2002@gmail.com>
2022-10-06 10:54:15 -0300

    Atividade - Q1 e Q2

---
+++ Exercícios/Exercícios_Lista_1.js
@@ -0,0 +1,34 @@
+//Exercícios após finalização da Seção 6
+
+/*
+//Questão 01
+function calc(x, y){
+    console.log(`Soma: ${x+y}`);
+    console.log(`Subtração: ${x-y}`);
+    console.log(`Multiplicação: ${x*y}`)
+    console.log(`Divisão: ${x/y}`)
+}
+*/
+
+/*
+//Questão 02
+
+function classificacao_triangulo(a, b, c){
+    lados_iguais = 0;
+    if (a == b){ lados_iguais += 1}
+    if (b == c){ lados_iguais += 1}
+    if (a == c){ lados_iguais += 1}
+
+    if(lados_iguais == 3){ console.log('O triângulo é Equilátero')}
+    else if(lados_iguais == 1){ console.log('O triângulo é Isósceles')}
+    else console.log('O triângulo é Escaleno')       
+}
+*/
+
+
+
+
+
+
+
+
