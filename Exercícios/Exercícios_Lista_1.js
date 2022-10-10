ae023ab4ede83075746aec65e17b736fd817bef9
Maxwell <maxwellpires2002@gmail.com>
2022-10-10 10:18:44 -0300

    Atividade Q15 e Q16

--- Exercícios/Exercícios_Lista_1.js
+++ Exercícios/Exercícios_Lista_1.js
@@ -192,6 +192,38 @@ function vendas(fruta){
 }
 */
 
+/*
+//Questão 15
+function concessionaria(carro){
+    switch(carro){
+        case 'hatch': console.log("Compra efetuada com sucesso"); break;
+        case 'sedan': 
+        case 'motocicleta': 
+        case 'caminhonete': console.log("Tem certeza que não prefere esse modelo?"); break;
+        default: console.log("Não trabalhamos com este tipo de automóvel aqui"); break;
+    }
+}
+*/
+
+/*
+//Questão 16
+function calculadora(a, operacao, b){
+    switch(operacao){
+        case "+": console.log(a+b);break;
+        case "-": console.log(a-b);break;
+        case '*': console.log(a*b); break;
+        case "/": console.log(a/b);break;
+        default: console.log("Operação Inválida!")
+    }
+}
+*/
+
+
+
+
+
+
+
 
 
 
