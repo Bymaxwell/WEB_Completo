cac0387067ded1ee6f1b64e68720fe6034faa8be
Maxwell <maxwellpires2002@gmail.com>
2022-10-17 10:42:34 -0300

    Revisão OO

--- Seção 7 - Orientação a Objetos/OO.js
+++ Seção 7 - Orientação a Objetos/OO.js
@@ -1,7 +1,7 @@
 // Código não executável
 
 //Procedural
-processamento(valor1, valor2, valor3)
+/*processamento(valor1, valor2, valor3)
 
 //OO
 objeto = {
@@ -12,11 +12,57 @@ objeto = {
 
 }
 
-objeto.processamento()
+objeto.processamento()*/
 
 //Princípios importantes:
 //1.Abstração
 //2. Encapsulamento
 //3. Herança -> Existe o reuso de código por Composição{TEM UM} e também por Herança{É UM}
 //4. Polimorfismo
+
+
+//Exemplos + Revisão
+//Objeto é uma coleção dinâmica de partes chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['Marca do produto'] = 'Genérica'
+produto.preco = 220
+
+console.log(produto)
+
+delete produto.preco
+delete produto['Marca do produto'] 
+
+console.log(produto)
+
+
+const carro = {
+    Modelo: 'A4',
+    Valor: 89000,
+    Proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        Endereco: {
+            Rua: 'Generica Rua',
+            Numero: 12,
+            Bairro: 'Genérico Bairro'
+        }
+    },
+    condutores: [
+        {
+            nome: 'Júnior', idade: 19
+        }, {
+            nome:'Maria', idade: 23
+        }],
+        calcularValorSeguro: function(){}
+}
+
+carro.Proprietario.Endereco.Numero = 1000
+carro['Proprietario']['Endereco']['Rua'] = 'Rua Um pouco mais Genérica'
+
+console.log(carro)
+
+delete carro.condutores
+delete carro.calcularValorSeguro
+console.log(carro)
 
