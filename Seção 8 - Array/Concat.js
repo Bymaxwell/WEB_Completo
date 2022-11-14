b947551dbe1cfc7998f58262e783e89b498615b8
Maxwell <maxwellpires2002@gmail.com>
2022-11-14 11:43:13 -0300

    Função Concat

---
+++ Seção 8 - Array/Concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Valeska', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+
+const todos_01 = filhos.concat('Fulano', filhas)
+const todos_02 = filhas.concat(filhos)
+
+console.log(todos_01)
+console.log(todos_02)
